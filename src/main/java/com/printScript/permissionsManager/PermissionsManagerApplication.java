b0ffd955ead86a99bac70dbf1d2d45bbc1f3392a
sonpipe0 <pipefc2003@gmail.com>
2024-09-24 23:02:28 -0300

    setup

---
+++ src/main/java/com/printScript/permissionsManager/PermissionsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.printScript.permissionsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermissionsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/printScript/permissionsManager/PermissionsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printScript.permissionsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
